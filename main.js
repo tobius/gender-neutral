83c4a6abf773652e5d0eb38e9f7784d1cee54ff8
Toby Miller <tobius@tobius.com>
2021-11-06 12:34:49 -0400

    Commit initial refactor/upgrade work
    - Upgrade from Node 6 to Node 12 (for ES6 support)
    - Remove `underscore` module (unused)
    - Remove `fs` module (unused)
    - Remove `natural` module (unused)
    - Remove `request` module (unused)
    - Remove `WNdb` module (unused)
    - Update whitespace in files to match `.editorconfig` rules
    - Add `eslint` module (for lint support)
    - Add `eslint-config-airbnb-base` module (for airbnb support)
    - Add `eslint-plugin-import` module (for import support)
    - Add `depcheck` module (for dependency checks)
    - Add `npm-check` module (for dependency upgrades)
    - Add `npm run dep-check` for dependency checks
    - Add `npm run dep-upgrade` for dependency upgrades
    - Add `npm run lint` for lint checks (use `eslint` and `airbnb`)
    - Give `chai` a semver
    - Give `async` a semver (to be refactored out entirely)

--- main.js
+++ main.js
@@ -1,313 +1,256 @@
-
-var _ = require('underscore'),
-    async = require('async'),
-    fs = require('fs'),
-    natural = require('natural'),
-    request = require('request');
-
-var inflector = new natural.NounInflector(),
-    wordnet = new natural.WordNet();
+// modules
+const async = require('async');
 
 var gender = module.exports = {
+  /**
+   * specific gender neutral patterns
+   *
+   * @var {Object}
+   * @name patterns
+   */
+  patterns: {
+    nominativeSubject    : /\b(he|she)\b/gi,
+    obliqueObject        : /\b(him|her)\b/gi,
+    possessiveDeterminer : /\b(his|her)\b/gi,
+    possessivePronoun    : /\b(his|hers)\b/gi,
+    reflexive            : /\b(himself|herself)\b/gi
+  },
 
-    /**
-     * specific gender neutral patterns
-     *
-     * @var {Object}
-     * @name patterns
-     */
-    patterns: {
-        nominativeSubject    : /\b(he|she)\b/gi,
-        obliqueObject        : /\b(him|her)\b/gi,
-        possessiveDeterminer : /\b(his|her)\b/gi,
-        possessivePronoun    : /\b(his|hers)\b/gi,
-        reflexive            : /\b(himself|herself)\b/gi
+  /**
+   * various gender neutral filters
+   *
+   * @var {Object}
+   * @name filters
+   */
+  filters: {
+    they: {
+      nominativeSubject    : 'they',
+      obliqueObject        : 'them',
+      possessiveDeterminer : 'their',
+      possessivePronoun    : 'theirs',
+      reflexive            : 'themself'
     },
-
-    /**
-     * various gender neutral filters
-     *
-     * @var {Object}
-     * @name filters
-     */
-    filters: {
-        they: {
-            nominativeSubject    : 'they',
-            obliqueObject        : 'them',
-            possessiveDeterminer : 'their',
-            possessivePronoun    : 'theirs',
-            reflexive            : 'themself'
-        },
-        e: {
-            nominativeSubject    : 'e',
-            obliqueObject        : 'em',
-            possessiveDeterminer : 'eir',
-            possessivePronoun    : 'eirs',
-            reflexive            : 'eirself'
-        },
-        ey: {
-            nominativeSubject    : 'ey',
-            obliqueObject        : 'em',
-            possessiveDeterminer : 'eir',
-            possessivePronoun    : 'eirs',
-            reflexive            : 'emself'
-        },
-        tho: {
-            nominativeSubject    : 'tho',
-            obliqueObject        : 'thor',
-            possessiveDeterminer : 'thors',
-            possessivePronoun    : 'thor',
-            reflexive            : 'thongself'
-        },
-        hu: {
-            nominativeSubject    : 'hu',
-            obliqueObject        : 'hum',
-            possessiveDeterminer : 'hus',
-            possessivePronoun    : 'hus',
-            reflexive            : 'humself'
-        },
-        per: {
-            nominativeSubject    : 'per',
-            obliqueObject        : 'per',
-            possessiveDeterminer : 'per',
-            possessivePronoun    : 'pers',
-            reflexive            : 'perself'
-        },
-        thon: {
-            nominativeSubject    : 'thon',
-            obliqueObject        : 'thon',
-            possessiveDeterminer : 'thons',
-            possessivePronoun    : 'thons',
-            reflexive            : 'thonself'
-        },
-        jee: {
-            nominativeSubject    : 'jee',
-            obliqueObject        : 'jem',
-            possessiveDeterminer : 'jeir',
-            possessivePronoun    : 'jeirs',
-            reflexive            : 'jemself'
-        },
-        ve: {
-            nominativeSubject    : 've',
-            obliqueObject        : 'ver',
-            possessiveDeterminer : 'vis',
-            possessivePronoun    : 'vis',
-            reflexive            : 'verself'
-        },
-        xe: {
-            nominativeSubject    : 'xe',
-            obliqueObject        : 'xem',
-            possessiveDeterminer : 'xyr',
-            possessivePronoun    : 'xyrs',
-            reflexive            : 'xemself'
-        },
-        ze: {
-            nominativeSubject    : 'ze',
-            obliqueObject        : 'mer',
-            possessiveDeterminer : 'zer',
-            possessivePronoun    : 'zers',
-            reflexive            : 'zemself'
-        },
-        zhe: {
-            nominativeSubject    : 'zhe',
-            obliqueObject        : 'zhim',
-            possessiveDeterminer : 'zher',
-            possessivePronoun    : 'zhers',
-            reflexive            : 'zhimself'
-        }
+    e: {
+      nominativeSubject    : 'e',
+      obliqueObject        : 'em',
+      possessiveDeterminer : 'eir',
+      possessivePronoun    : 'eirs',
+      reflexive            : 'eirself'
     },
-
-    /**
-     * perform a safe regexp replacement that preserves capitalization
-     *
-     * @param {String} text
-     * @param {RegExp} find
-     * @param {String} replace
-     */
-    safeReplace: function(text, find, replace){
-
-        text = text.replace(find, function(match, specific){
-            if (/[A-Z]/.test(specific.substring(0, 1))){
-                return replace.charAt(0).toUpperCase() + replace.substring(1);
-            }
-            return replace;
-        });
-        return text;
-
+    ey: {
+      nominativeSubject    : 'ey',
+      obliqueObject        : 'em',
+      possessiveDeterminer : 'eir',
+      possessivePronoun    : 'eirs',
+      reflexive            : 'emself'
     },
-
-    /**
-     * determine if a word could be a noun
-     *
-     * @param {String} word
-     * @param {Function} callback({String} err, {Boolean} result)
-     */
-    /*
-    isPossibleNoun: function(word, callback){
-
-        var possible = false;
-
-        word = inflector.singularize(word);
-
-        if (word){
-
-            wordnet.lookup(word, function(results){
-                for (var i = results.length - 1; i > -1; i -= 1){
-                    if (results[i].pos === 'n'){
-                        possible = true;
-                    }
-                }
-                callback(undefined, possible);
-            });
-
-        } else {
-
-            callback('unable to identify word: ' + word);
-
-        }
-
+    tho: {
+      nominativeSubject    : 'tho',
+      obliqueObject        : 'thor',
+      possessiveDeterminer : 'thors',
+      possessivePronoun    : 'thor',
+      reflexive            : 'thongself'
     },
-    */
-
-    /**
-     * neutralize gender specific nominative subjects
-     *
-     * example: He/She laughed
-     *
-     * @param {String} text
-     * @param {Function} callback({String} err, {String} text)
-     * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
-     */
-    neutralizeNominativeSubjects: function(text, callback){
-
-        var filter, past, present;
-
-        // terms
-        filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
-        text = this.safeReplace(text, this.patterns.nominativeSubject, this.filters[filter].nominativeSubject);
-
-        // tense
-        past = new RegExp('\\b(' + this.filters[filter].nominativeSubject + ') +was\\b', 'i');
-        present = new RegExp('\\b(' + this.filters[filter].nominativeSubject + ') +is\\b', 'i');
-        text = text.replace(past, '$1 were');
-        text = text.replace(present, '$1 are');
-
-        // finish
-        callback(undefined, text);
-
+    hu: {
+      nominativeSubject    : 'hu',
+      obliqueObject        : 'hum',
+      possessiveDeterminer : 'hus',
+      possessivePronoun    : 'hus',
+      reflexive            : 'humself'
     },
-
-    /**
-     * neutralize gender specific oblique objects
-     *
-     * example: I called him/her
-     *
-     * @param {String} text
-     * @param {Function} callback({String} err, {String} text)
-     * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
-     */
-    neutralizeObliqueObjects: function(text, callback){
-
-        var filter;
-
-        // terms
-        filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
-        text = this.safeReplace(text, this.patterns.obliqueObject, this.filters[filter].obliqueObject);
-
-        // finish
-        callback(undefined, text);
-
+    per: {
+      nominativeSubject    : 'per',
+      obliqueObject        : 'per',
+      possessiveDeterminer : 'per',
+      possessivePronoun    : 'pers',
+      reflexive            : 'perself'
     },
-
-    /**
-     * neutralize gender specific possessive determiners
-     *
-     * example: His/Her eyes gleam
-     *
-     * @param {String} text
-     * @param {Function} callback({String} err, {String} text)
-     * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
-     */
-    neutralizePossessiveDeterminers: function(text, callback){
-
-        var filter;
-
-        // terms
-        filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
-        text = this.safeReplace(text, this.patterns.possessiveDeterminer, this.filters[filter].possessiveDeterminer);
-
-        // finish
-        callback(undefined, text);
-
+    thon: {
+      nominativeSubject    : 'thon',
+      obliqueObject        : 'thon',
+      possessiveDeterminer : 'thons',
+      possessivePronoun    : 'thons',
+      reflexive            : 'thonself'
     },
-
-    /**
-     * neutralize gender specific possessive pronouns
-     *
-     * example: That is his/hers
-     *
-     * @param {String} text
-     * @param {Function} callback({String} err, {String} text)
-     * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
-     */
-    neutralizePossessivePronouns: function(text, callback){
-
-        var filter;
-
-        // terms
-        filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
-        text = this.safeReplace(text, this.patterns.possessivePronoun, this.filters[filter].possessivePronoun);
-
-        // finish
-        callback(undefined, text);
-
+    jee: {
+      nominativeSubject    : 'jee',
+      obliqueObject        : 'jem',
+      possessiveDeterminer : 'jeir',
+      possessivePronoun    : 'jeirs',
+      reflexive            : 'jemself'
     },
-
-    /**
-     * neutralize gender specific reflexives
-     *
-     * example: He/She likes himself/herself
-     *
-     * @param {String} text
-     * @param {Function} callback({String} err, {String} text)
-     * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
-     */
-    neutralizeReflexives: function(text, callback){
-
-        var filter;
-
-        // terms
-        filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
-        text = this.safeReplace(text, this.patterns.reflexive, this.filters[filter].reflexive);
-
-        // finish
-        callback(undefined, text);
-
+    ve: {
+      nominativeSubject    : 've',
+      obliqueObject        : 'ver',
+      possessiveDeterminer : 'vis',
+      possessivePronoun    : 'vis',
+      reflexive            : 'verself'
     },
-
-    /**
-     * neutralize gender specific pronouns
-     *
-     * @param {String} text
-     * @param {Function} callback({String} err, {String} text)
-     * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
-     */
-    neutralize: function(text, callback){
-
-        var delegate, filter;
-
-        delegate = this;
-        filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
-
-        async.waterfall([
-            function(callback){ delegate.neutralizeNominativeSubjects(text, callback); },
-            function(text, callback){ delegate.neutralizeObliqueObjects(text, callback); },
-            function(text, callback){ delegate.neutralizePossessiveDeterminers(text, callback); },
-            function(text, callback){ delegate.neutralizePossessivePronouns(text, callback); },
-            function(text, callback){ delegate.neutralizeReflexives(text, callback); },
-        ], callback);
-
+    xe: {
+      nominativeSubject    : 'xe',
+      obliqueObject        : 'xem',
+      possessiveDeterminer : 'xyr',
+      possessivePronoun    : 'xyrs',
+      reflexive            : 'xemself'
+    },
+    ze: {
+      nominativeSubject    : 'ze',
+      obliqueObject        : 'mer',
+      possessiveDeterminer : 'zer',
+      possessivePronoun    : 'zers',
+      reflexive            : 'zemself'
+    },
+    zhe: {
+      nominativeSubject    : 'zhe',
+      obliqueObject        : 'zhim',
+      possessiveDeterminer : 'zher',
+      possessivePronoun    : 'zhers',
+      reflexive            : 'zhimself'
     }
+  },
 
+  /**
+   * perform a safe regexp replacement that preserves capitalization
+   *
+   * @param {String} text
+   * @param {RegExp} find
+   * @param {String} replace
+   */
+  safeReplace: function(text, find, replace){
+    text = text.replace(find, function(match, specific){
+      if (/[A-Z]/.test(specific.substring(0, 1))){
+        return replace.charAt(0).toUpperCase() + replace.substring(1);
+      }
+      return replace;
+    });
+    return text;
+  },
+
+  /**
+   * neutralize gender specific nominative subjects
+   *
+   * example: He/She laughed
+   *
+   * @param {String} text
+   * @param {Function} callback({String} err, {String} text)
+   * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
+   */
+  neutralizeNominativeSubjects: function(text, callback){
+    var filter, past, present;
+
+    // terms
+    filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
+    text = this.safeReplace(text, this.patterns.nominativeSubject, this.filters[filter].nominativeSubject);
+
+    // tense
+    past = new RegExp('\\b(' + this.filters[filter].nominativeSubject + ') +was\\b', 'i');
+    present = new RegExp('\\b(' + this.filters[filter].nominativeSubject + ') +is\\b', 'i');
+    text = text.replace(past, '$1 were');
+    text = text.replace(present, '$1 are');
+
+    // finish
+    callback(undefined, text);
+  },
+
+  /**
+   * neutralize gender specific oblique objects
+   *
+   * example: I called him/her
+   *
+   * @param {String} text
+   * @param {Function} callback({String} err, {String} text)
+   * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
+   */
+  neutralizeObliqueObjects: function(text, callback){
+    var filter;
+
+    // terms
+    filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
+    text = this.safeReplace(text, this.patterns.obliqueObject, this.filters[filter].obliqueObject);
+
+    // finish
+    callback(undefined, text);
+  },
+
+  /**
+   * neutralize gender specific possessive determiners
+   *
+   * example: His/Her eyes gleam
+   *
+   * @param {String} text
+   * @param {Function} callback({String} err, {String} text)
+   * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
+   */
+  neutralizePossessiveDeterminers: function(text, callback){
+    var filter;
+
+    // terms
+    filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
+    text = this.safeReplace(text, this.patterns.possessiveDeterminer, this.filters[filter].possessiveDeterminer);
+
+    // finish
+    callback(undefined, text);
+  },
+
+  /**
+   * neutralize gender specific possessive pronouns
+   *
+   * example: That is his/hers
+   *
+   * @param {String} text
+   * @param {Function} callback({String} err, {String} text)
+   * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
+   */
+  neutralizePossessivePronouns: function(text, callback){
+    var filter;
+
+    // terms
+    filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
+    text = this.safeReplace(text, this.patterns.possessivePronoun, this.filters[filter].possessivePronoun);
+
+    // finish
+    callback(undefined, text);
+  },
+
+  /**
+   * neutralize gender specific reflexives
+   *
+   * example: He/She likes himself/herself
+   *
+   * @param {String} text
+   * @param {Function} callback({String} err, {String} text)
+   * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
+   */
+  neutralizeReflexives: function(text, callback){
+    var filter;
+
+    // terms
+    filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
+    text = this.safeReplace(text, this.patterns.reflexive, this.filters[filter].reflexive);
+
+    // finish
+    callback(undefined, text);
+  },
+
+  /**
+   * neutralize gender specific pronouns
+   *
+   * @param {String} text
+   * @param {Function} callback({String} err, {String} text)
+   * @param {String} [filter=they] (they|e|ey|tho|hu|per|thon|jee|ve|xe|ze|zhe)
+   */
+  neutralize: function(text, callback){
+    var delegate, filter;
+
+    delegate = this;
+    filter = (arguments > 2 && filters[arguments[2]] !== undefined) ? arguments[2] : 'they';
+
+    async.waterfall([
+      function(callback){ delegate.neutralizeNominativeSubjects(text, callback); },
+      function(text, callback){ delegate.neutralizeObliqueObjects(text, callback); },
+      function(text, callback){ delegate.neutralizePossessiveDeterminers(text, callback); },
+      function(text, callback){ delegate.neutralizePossessivePronouns(text, callback); },
+      function(text, callback){ delegate.neutralizeReflexives(text, callback); },
+    ], callback);
+  }
 };
-
--- test/gender-neutral.js
+++ test/gender-neutral.js
@@ -1,4 +1,3 @@
-
 var assert, chai, gender, withGender, withoutGender;
 
 chai = require('chai');
@@ -8,67 +7,66 @@ chai.Assertion.stackTrace = true;
 
 assert = chai.assert;
 
-describe('require("gender-neutral")', function(){
+describe('require("gender-neutral")', function () {
 
-    describe('gender.neutralizeNominativeSubjects(text, callback, [type])', function(){
-        it('should return neutralized nominative subjects for gender specific statement', function(done){
-            var text = 'She solved the problem by combining the proper solvents according to the prescribed ratio.';
-            gender.neutralizeNominativeSubjects(text, function(err, neutral){
-                assert(neutral === 'They solved the problem by combining the proper solvents according to the prescribed ratio.', neutral);
-                done();
-            }, 'they');
-        });
+  describe('gender.neutralizeNominativeSubjects(text, callback, [type])', function () {
+    it('should return neutralized nominative subjects for gender specific statement', function (done) {
+      var text = 'She solved the problem by combining the proper solvents according to the prescribed ratio.';
+      gender.neutralizeNominativeSubjects(text, function (err, neutral) {
+        assert(neutral === 'They solved the problem by combining the proper solvents according to the prescribed ratio.', neutral);
+        done();
+      }, 'they');
     });
+  });
 
-    describe('gender.neutralizeObliqueObjects(text, callback, [type])', function(){
-        it('should return neutralized oblique objects for gender specific statements', function(done){
-            var text = 'I spoke to him that morning and told him that the fervor quotient was the way to go.';
-            gender.neutralizeObliqueObjects(text, function(err, neutral){
-                assert(neutral === 'I spoke to them that morning and told them that the fervor quotient was the way to go.', neutral);
-                done();
-            }, 'they');
-        });
+  describe('gender.neutralizeObliqueObjects(text, callback, [type])', function () {
+    it('should return neutralized oblique objects for gender specific statements', function (done) {
+      var text = 'I spoke to him that morning and told him that the fervor quotient was the way to go.';
+      gender.neutralizeObliqueObjects(text, function (err, neutral) {
+        assert(neutral === 'I spoke to them that morning and told them that the fervor quotient was the way to go.', neutral);
+        done();
+      }, 'they');
     });
+  });
 
-    describe('gender.neutralizePossessiveDeterminers(text, callback, [type])', function(){
-        it('should return neutralized possessive determiners for gender specific statements', function(done){
-            var text = 'His back was so freaking wide that he looked like the Hulk from the rear.';
-            gender.neutralizePossessiveDeterminers(text, function(err, neutral){
-                assert(neutral === 'Their back was so freaking wide that he looked like the Hulk from the rear.', neutral);
-                done();
-            }, 'they');
-        });
+  describe('gender.neutralizePossessiveDeterminers(text, callback, [type])', function () {
+    it('should return neutralized possessive determiners for gender specific statements', function (done) {
+      var text = 'His back was so freaking wide that he looked like the Hulk from the rear.';
+      gender.neutralizePossessiveDeterminers(text, function (err, neutral) {
+        assert(neutral === 'Their back was so freaking wide that he looked like the Hulk from the rear.', neutral);
+        done();
+      }, 'they');
     });
+  });
 
-    describe('gender.neutralizePossessivePronouns(text, callback, [type])', function(){
-        it('should return neutralized possessive pronouns for gender specific statements', function(done){
-            var text = 'I took the toy away from him and told him that it was hers and that he was not to touch it again.';
-            gender.neutralizePossessivePronouns(text, function(err, neutral){
-                assert(neutral === 'I took the toy away from him and told him that it was theirs and that he was not to touch it again.', neutral);
-                done();
-            }, 'they');
-        });
+  describe('gender.neutralizePossessivePronouns(text, callback, [type])', function () {
+    it('should return neutralized possessive pronouns for gender specific statements', function (done) {
+      var text = 'I took the toy away from him and told him that it was hers and that he was not to touch it again.';
+      gender.neutralizePossessivePronouns(text, function (err, neutral) {
+        assert(neutral === 'I took the toy away from him and told him that it was theirs and that he was not to touch it again.', neutral);
+        done();
+      }, 'they');
     });
+  });
 
-    describe('gender.neutralizeReflexives(text, callback, [type])', function(){
-        it('should return neutralized reflexives for gender specific statements', function(done){
-            var text = 'She looked into the mirror and admired herself every morning ... she loved being a Mermaid.';
-            gender.neutralizeReflexives(text, function(err, neutral){
-                assert(neutral === 'She looked into the mirror and admired themself every morning ... she loved being a Mermaid.', neutral);
-                done();
-            }, 'they');
-        });
+  describe('gender.neutralizeReflexives(text, callback, [type])', function () {
+    it('should return neutralized reflexives for gender specific statements', function (done) {
+      var text = 'She looked into the mirror and admired herself every morning ... she loved being a Mermaid.';
+      gender.neutralizeReflexives(text, function (err, neutral) {
+        assert(neutral === 'She looked into the mirror and admired themself every morning ... she loved being a Mermaid.', neutral);
+        done();
+      }, 'they');
     });
+  });
 
-    describe('gender.neutralize(text, callback, [type])', function(){
-        it('should return neutralized text for gender specific statements', function(done){
-            var text = 'I called him on Wednesday to tell him that she laughed at the singing Teddy Bear that he got her. Her eyes were overfilled with joy everytime it said "That is her! That is my new friend!". She really seems to like herself today.';
-            gender.neutralize(text, function(err, neutral){
-                assert(neutral === 'I called them on Wednesday to tell them that they laughed at the singing Teddy Bear that they got them. Their eyes were overfilled with joy everytime it said "That is them! That is my new friend!". They really seemed to like themself today.', neutral);
-                done();
-            }, 'they');
-        });
+  describe('gender.neutralize(text, callback, [type])', function () {
+    it('should return neutralized text for gender specific statements', function (done) {
+      var text = 'I called him on Wednesday to tell him that she laughed at the singing Teddy Bear that he got her. Her eyes were overfilled with joy everytime it said "That is her! That is my new friend!". She really seems to like herself today.';
+      gender.neutralize(text, function (err, neutral) {
+        assert(neutral === 'I called them on Wednesday to tell them that they laughed at the singing Teddy Bear that they got them. Their eyes were overfilled with joy everytime it said "That is them! That is my new friend!". They really seemed to like themself today.', neutral);
+        done();
+      }, 'they');
     });
+  });
 
 });
-
